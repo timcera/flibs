13c4f57209fc627b7f08e6cbad9f40c2076150c9
knystrom <>
2013-12-30 13:47:05 +0000

    Adapting to type bound log_t configure/cget

--- tests/reporting/test_m_multilog.f90
+++ tests/reporting/test_m_multilog.f90
@@ -1,17 +1,17 @@
 ! test_m_multilog.f90 --
-!     Small test program for the multiple logs utility. Basted on the
+!     Small test program for the multiple logs utility. Based on the
 !     test program for m_logger.
 !
-!     Karin Nyström 2012
+!     Karin Nyström 2012 - 2013
 !
 program test_multilogging
   use m_multilog
   implicit none
 
-  character (len=500) :: msg
-  integer             :: logunit, i, infolevel
-  integer, pointer    :: stop_program => null()
-  type (log_t)        :: log_fine, log_info, log_warning, log_error
+  character(len=500) :: msg
+  integer            :: logunit, i, infolevel
+  integer, pointer   :: stop_program => null()
+  type(log_t)        :: log_fine, log_info, log_warning, log_error
   !
   ! Initialize the log files. Log without explicit info_lvl should be
   ! assigned infolevel=INFO. append=.false. to get rid of previous test
@@ -31,26 +31,36 @@ program test_multilogging
   call add_log( log_warning )
   call add_log( log_error )
   !
-  ! Message with and without time stamp written to all four logs. 
+  ! Message with and without timestamp written to all four logs. 
   ! 
   write (*,*) 'Writing two messages to all logs'
-  call log_configure( "timestamp" , .true. )
-  call log_msg( 'First message, with timestamp', ALL )
-  call log_configure( "timestamp" , .false. )
-  call log_msg( 'Second message, without timestamp', ALL )
+  write (*,*) 'Changing log configurations globally'
+  call log_group_configure( "timestamp", .true. )
+  call log_msg( 'First message, with default timestamp' )
+  call log_group_configure( "timestamp", .false. )
+  call log_msg( 'Second message, without timestamp' )
+  !
+  ! Enable/disable and change timestamp for a single log.
+  !
+  call log_info%configure( "timestamp", .true. )
+  call log_msg( 'This message should have timestamp only in log_info' )
+  call log_info%configure( "timefmt", "HH:MM:SS" )
+  call log_msg( 'This message has a custom timestamp in log_info' )
+  call log_info%configure( "timestamp", .false. )
+  call log_msg( 'Message without timestamp to all logs' )
   !
   ! Check that all logs have the appropriate infolevels
   !
-  call log_cget( log_fine, "infolevel", infolevel )
+  call log_fine%cget( "infolevel", infolevel )
   write( msg, '(a,i1,a,i1)') "log_fine has infolevel ", infolevel, ", FINE=", FINE
   call log_msg( msg )
-  call log_cget( log_info, "infolevel", infolevel )
+  call log_info%cget( "infolevel", infolevel )
   write( msg, '(a,i1,a,i1)') "log_info has infolevel ", infolevel, ", INFO=", INFO
   call log_msg( msg )
-  call log_cget( log_warning, "infolevel", infolevel )
+  call log_warning%cget( "infolevel", infolevel )
   write( msg, '(a,i1,a,i1)') "log_warning has infolevel ", infolevel, ", WARNING=", WARNING
   call log_msg( msg )
-  call log_cget( log_error, "infolevel", infolevel )
+  call log_error%cget( "infolevel", infolevel )
   write( msg, '(a,i1,a,i1)') "log_error has infolevel ", infolevel, ", ERROR=", ERROR
   call log_msg( msg )
   !
@@ -58,9 +68,9 @@ program test_multilogging
   !
   write (*,*) 'Closing log_fine, removing from logger, restarting with append = .false., '
   write (*,*) 'adding to logger and writing a message to that log only'
-  call remove_log ( log_fine )
+  call remove_log( log_fine )
   call log_fine%startup( 'test_m_multilog_fine.log', append=.false., info_lvl=FINE)
-  call add_log ( log_fine )
+  call add_log( log_fine )
   call log_msg( 'Third message - should be the first row now', FINE )
   !
   ! Write one message per info level. ERROR should be written to all
@@ -90,34 +100,34 @@ program test_multilogging
   ! Get the log unit and write directly to the logfile
   !
   write (*,*) 'Get unit number for log_warning and write to that'
-  call log_cget ( log_warning, "logfileunit" , logunit)
+  call log_warning%cget( "logfileunit" , logunit )
   write ( logunit , "(A)") "This is my manually written message, written only to log_warning's file."
   !
   ! Enable / disable writing on standard output. When no info
   ! level is supplied to log_msg, INFO should be used.
   !
   write (*,*) 'Write one message only to file.'
-  call log_configure ( "writeonstdout" , .false. )
+  call log_group_configure( "writeonstdout" , .false. )
   call log_msg( 'This message is written only on file' )
   write (*,*) 'Setting writestdout to .true. again'
-  call log_configure ( "writeonstdout" , .true. )
+  call log_group_configure( "writeonstdout" , .true. )
   call log_msg( 'This message is written both on screen and on file' )
   !
   ! Shut down one of the logs and write that to info_log.
   ! 
   write (*,*) 'Removing log_fine and writing message to log_info'
-  call remove_log ( log_fine )
+  call remove_log( log_fine )
   call log_msg( "log_fine shut down, now this (log_info) is the most detailed log.", INFO)
   !
   ! Shut down the rest of the logs.
   !
   write (*,*) 'Closing down rest of the logs and the log group.'
-  call shutdown_log_group ()
+  call shutdown_log_group()
   !
   ! Abort the program to test that the log file is properly flushed
   !
   call log_info%startup( 'test_m_multilog_info.log' )
-  call add_log ( log_info )
+  call add_log( log_info )
   call log_msg( 'Before expressly aborting the program' )
   write( *, '(a)' ) ' '
   write( *, '(a)' ) 'Warning: the program will abort now - this is intentional'
